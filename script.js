e4704885c554446f6bb26cf8a45477d83fbf4068
Kyrylo Sheludchenko <kirill.shelydchenko@gmail.com>
2025-05-14 17:24:59 +0200

    to boolean

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ checkTypes(undefined, {});
 
 function toNumber(val) {
   return {
+    input: val,
     number: Number(val),
     parse: parseInt(val),
     unary: +val,
   };
 }
 
-console.log(toNumber("4"));
-console.log(toNumber(""));
+console.log(toNumber("123"));
+console.log(toNumber("123abc"));
+console.log(toNumber(true));
 console.log(toNumber(null));
+
+function testTruthyFalsy(val) {
+  return {
+    input: val,
+    bool: Boolean(val),
+    trenary: val ? "thruthy" : "falsy",
+  };
+}
+
+console.log(testTruthyFalsy(0));
+console.log(testTruthyFalsy(""));
+console.log(testTruthyFalsy(" "));
+console.log(testTruthyFalsy([]));
+console.log(testTruthyFalsy({}));
+console.log(testTruthyFalsy(null));
